55f97e9142178b52cd5e21327cec405c7ea79041
shelle-mim <michelleleong8@gmail.com>
2023-10-04 23:48:02 +0800

    skeleton profile css

--- src/views/Profile/ProfileView.vue
+++ src/views/Profile/ProfileView.vue
@@ -1,19 +1,61 @@
 <script setup>
 import Tab from '../../components/Tab/Tab.vue'
 import TabsWrapper from '../../components/Tab/TabsWrapper.vue'
+import Button from 'primevue/Button';
+
+import { ref } from 'vue'
 
 const showLog = () => console.log('helloooo')
+
+const profileInfo = ref({
+  name: 'Peter Parker',
+  username: '@peterparker',
+  description: 'Hello people!  I’m a student from SMU! It’s Singapore Management University in case you don’t know! I love to volunteer during my free time.  I also do giveaways as well. ',
+  giveaways: 186,
+  requests: 97,
+})
+
 </script>
 
 <template>
   <main class="remove-padding">
     <section class="profile">
       <div class="profile-content">
-        <div class="profile-photo">Photo</div>
-
-        <div class="profile-info">Info</div>
-
-        <div class="profile-statistics">Statistics</div>
+        <div class="profile-photo pi pi-user"></div>
+        <div class="profile-info">
+          <div style="display: flex; justify-content: space-between; padding-bottom: 20px;">
+            <div>
+              <h1>{{ profileInfo.name }}</h1>
+              <p>{{ profileInfo.username }}</p>
+            </div>
+            <Button type="button" class="edit-button" label="Edit Profile" icon="pi pi-cog"/>
+          </div>
+          <p style="padding-bottom: 20px;">{{ profileInfo.description }}</p>
+          <Button class="dietary-restrictions">Dietary Restrictions</Button>
+        </div>
+        <div class="profile-statistics">
+          <div style="width: fill-available; display: flex; align-items: center; justify-content: space-between;">
+            <div style="display: flex;">
+              <div class="statistics-icon-wrapper">
+                <div><Button class="icon" icon="pi pi-star"/></div>
+                <div>
+                  <h3>{{ profileInfo.giveaways }}</h3>
+                  <p>Giveaways Made</p>
+                </div>
+              </div>
+              <div class="statistics-icon-wrapper">
+                <div><Button class="icon" icon="pi pi-download"/></div>
+                <div>
+                  <h3>{{ profileInfo.requests }}</h3>
+                  <p>Requests Made</p>
+                </div>
+              </div>
+            </div>
+            <div>
+              <Button type="button" class="statistics-button" label="View Statistics" icon="pi pi-chart-bar"/>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
     <section class="my-listings">
@@ -25,61 +67,109 @@ const showLog = () => console.log('helloooo')
           <div class="container pt-small">Requests</div>
         </Tab>
       </TabsWrapper>
-
-      <!-- <TabView class="tabview-custom">
-        <TabPanel>
-          <template #header>
-            <i class="pi pi-gift mr-2 text-2xl"></i>
-            <span>Giveaways</span>
-          </template>
-          <div class="container">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-            dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-            mollit anim id est laborum.
-          </div>
-        </TabPanel>
-        <TabPanel>
-          <template #header>
-            <i class="pi pi-megaphone mr-2 text-2xl"></i>
-            <span>Requests</span>
-          </template>
-          <div class="container">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
-            laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-            architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas
-            sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-            voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi.
-          </div>
-        </TabPanel>
-      </TabView> -->
     </section>
   </main>
 </template>
 
 <style scoped>
+
+.statistics-icon-wrapper {
+  display: flex;
+  width: 150px;
+  justify-content: center;
+  align-items: center;
+  margin-right: 20px;
+}
+.statistics-button {
+  background:white;
+  color: black;
+  border-radius: 100px;
+  border-color: black;
+}
+.edit-button {
+  background:grey;
+  color: white;
+  border-radius: 100px;
+  border: none;
+}
 .profile {
+  display: flex;
   width: 100%;
-  min-height: 450px;
   height: 100%;
-  background: lightgray;
+  background: whitesmoke;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .profile-content {
   display: grid;
-  grid-template-areas: '';
+  grid-template-areas:
+    'profile-photo profile-info profile-info'
+    'statistics statistics statistics';
+  gap: 30px;
+  padding: 10px;
+  width: 600px;
+  justify-content: center;
 }
 
 .profile-photo {
   grid-area: profile-photo;
+  background: darkgray;
+  height: 200px;
+  width: 200px;
+  border-radius: 200px;
+  line-height: 200px;
+  text-align: center;
+  font-size: 8rem;
 }
 .profile-info {
   grid-area: profile-info;
+  width: fill-available;
 }
 .profile-statistics {
   grid-area: statistics;
+  /* background: plum; */
+}
+
+@media only screen and (max-width: 600px) {
+  .profile-content {
+    grid-template-areas:
+      'profile-photo statistics'
+      'profile-info profile-info';
+    align-items: center;
+  }
+  .profile-photo {
+    height: 150px;
+    width: 150px;
+    line-height: 150px;
+    font-size: 6rem;
+  }
+  .icon {
+    display: none
+  }
+  .statistics-button {
+    font-size: 0;
+  }
+  .wrapper {
+    width: 100px;
+  }
+}
+Button.icon {
+  background: lightgray;
+  color:black;
+  border-radius: 100%;
+  outline: none;
+  border: none;
+  margin-right: 15px;
+}
+Button.dietary-restrictions {
+  color:black;
+  border-color: black;
+  background: none;
+  border-radius: 50px;
+  outline: none;
 }
 
 /*******************
