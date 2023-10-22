442cb8fa85cb13152f53169a9b6c66da03de85b3
shelle-mim <michelleleong8@gmail.com>
2023-10-23 01:33:22 +0800

    add static carma chart

--- src/views/Profile/ProfileView.vue
+++ src/views/Profile/ProfileView.vue
@@ -6,6 +6,7 @@ import Button from 'primevue/Button';
 import Avatar from 'primevue/Avatar';
 import Dialog from 'primevue/dialog';
 import Tag from 'primevue/tag';
+import Chart from 'primevue/chart';
 import { ref, onMounted } from 'vue'
 import { LISTING_TYPE } from '../../util/constants'
 
@@ -18,8 +19,9 @@ import { useUserStore } from '../../stores/user'
 // user info consts from db
 const user = useUserStore()
 const profileInfo = ref({})
-const num_giveaways = ref({})
-const num_requests = ref({})
+const num_giveaways = ref(0)
+const num_requests = ref(0)
+const carmaHistory = [5, 10, 15, 10, 20]
 
 // fetch user listings from db for giveaway and request count
 async function getListings() {
@@ -29,7 +31,6 @@ async function getListings() {
   if (error) {
     console.log('error: ', error)
   } else {
-    console.log(data)
     // filter out the giveaways and requests
     var giveaways = data.filter(listing => {
       return listing.listingType === LISTING_TYPE.Giveaway
@@ -42,16 +43,95 @@ async function getListings() {
   }
 }
 
+// Carma chart
+const chartData = ref();
+const chartOptions = ref();
+
+let getMonths = () => {
+  var dateObj = new Date();
+  var dateStrings = [];
+  var currDateMonth = new Date().getMonth() + 1
+  var dateFormatOptions = {}
+
+  if (carmaHistory.length <= currDateMonth + 1) {
+    dateFormatOptions = {
+      month: 'short',
+    };
+  } else {
+    dateFormatOptions = {
+      month: 'short',
+      year: 'numeric'
+    };
+  }
+
+  for (var i = 0; i < carmaHistory.length; ++i) {
+    dateStrings.unshift(dateObj.toLocaleString('en-US', dateFormatOptions));
+    dateObj.setMonth(dateObj.getMonth() - 1);
+  }
+
+  return dateStrings;
+}
+
+const setChartData = () => {
+  // Add curr month data to carmaHistory
+  var carmaData = carmaHistory
+  carmaData.push(profileInfo.value.monthlyCarma)
+
+  return {
+    labels: getMonths(),
+    datasets: [
+      {
+        label: 'Carma Points',
+        data: carmaData,
+        backgroundColor: 'rgba(246,132,56, 0.5)',
+        borderColor: '#f68438',
+        borderWidth: 1
+      }
+    ]
+  };
+};
+
+const setChartOptions = () => {
+  const documentStyle = getComputedStyle(document.documentElement);
+  const textColor = documentStyle.getPropertyValue('--text-color');
+  const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+  const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+  return {
+    plugins: { legend: { labels: { color: textColor } } },
+    scales: {
+      x: {
+        ticks: { color: textColorSecondary },
+        grid: { color: surfaceBorder }
+      },
+      y: {
+        beginAtZero: true,
+        ticks: { color: textColorSecondary },
+        grid: { color: surfaceBorder }
+      }
+    }
+  };
+}
+
 onMounted(() => {
   profileInfo.value = user.profile
   getListings()
+  chartData.value = setChartData();
+  chartOptions.value = setChartOptions();
 })
 
 const tabletScreen = useMatchMedia(screenSize.tablet)
 
-// For modal
-var visible = ref(false);
-let toggleModal = () => visible.value = !visible.value;
+// For modals
+var dietary_restrictions_visible = ref(false);
+var statistics_visible = ref(false);
+let toggleModal = () => {
+  if (dietary_restrictions_visible.value) {
+    dietary_restrictions_visible.value = !dietary_restrictions_visible.value;
+  } else if (statistics_visible.value) {
+    statistics_visible.value = !statistics_visible.value;
+  }
+}
 
 </script>
 
@@ -60,30 +140,31 @@ let toggleModal = () => visible.value = !visible.value;
     <section class="profile">
       <div class="profile-content">
         <Avatar v-if="profileInfo.avatarUrl" :image=profileInfo.avatarUrl class="profile-photo" shape="circle" />
-        <Avatar v-else :label="`${profileInfo?.username ? profileInfo.username.charAt(0).toUpperCase() : ''}`" class="profile-photo" shape="circle"/>
+        <Avatar v-else :label="`${profileInfo?.username ? profileInfo.username.charAt(0).toUpperCase() : ''}`"
+          class="profile-photo" shape="circle" />
         <div class="profile-info">
           <div style="display: flex; justify-content: space-between; padding-bottom: 20px;">
             <div>
               <h1>{{ profileInfo.username }}</h1>
               <p style="color: var(--color-primary)">@{{ profileInfo.handle }}</p>
             </div>
-            <Button type="button" class="edit-button" label="Edit Profile" icon="pi pi-cog"/>
+            <Button type="button" class="edit-button" label="Edit Profile" icon="pi pi-cog" />
           </div>
           <p style="padding-bottom: 20px;">{{ profileInfo.description }}</p>
-          <Button class="dietary-restrictions" @click="visible = true">Dietary Preferences</Button>
+          <Button class="dietary-restrictions" @click="dietary_restrictions_visible = true">Dietary Restrictions</Button>
         </div>
         <div class="profile-statistics">
           <div style="width: fill-available; display: flex; align-items: center; justify-content: space-between;">
             <div style="display: flex;">
               <div class="statistics-icon-wrapper">
-                <div><Button class="icon" icon="pi pi-star"/></div>
+                <div><Button class="icon" icon="pi pi-star" /></div>
                 <div>
                   <h3>{{ num_giveaways }}</h3>
                   <p>Giveaways Made</p>
                 </div>
               </div>
               <div class="statistics-icon-wrapper">
-                <div><Button class="icon" icon="pi pi-download"/></div>
+                <div><Button class="icon" icon="pi pi-download" /></div>
                 <div>
                   <h3>{{ num_requests }}</h3>
                   <p>Requests Made</p>
@@ -91,8 +172,10 @@ let toggleModal = () => visible.value = !visible.value;
               </div>
             </div>
             <div>
-              <Button v-if="tabletScreen" type="button" class="statistics-button" icon="pi pi-chart-bar"/>
-              <Button v-else type="button" class="statistics-button" label="View Statistics" icon="pi pi-chart-bar"/>
+              <Button v-if="tabletScreen" type="button" class="statistics-button" @click="statistics_visible = true"
+                icon="pi pi-chart-bar" />
+              <Button v-else type="button" class="statistics-button" @click="statistics_visible = true"
+                label="View Statistics" icon="pi pi-chart-bar" />
             </div>
           </div>
         </div>
@@ -110,26 +193,47 @@ let toggleModal = () => visible.value = !visible.value;
     </section>
   </main>
 
-  <div v-if="visible" class="modal" @click="toggleModal"></div>
-  <Dialog :visible="visible" :header="`${profileInfo.username}'s Dietary Preferences`" :style="{ width: '50vw' }">
-        <h3>Dietary Restrictions</h3>
-        <div v-if="profileInfo.dietaryRestrictions">
-          <Tag class="category tag" :value="profileInfo.dietaryRestrictions"></Tag>
+  <div v-if="dietary_restrictions_visible || statistics_visible" class="modal" @click="toggleModal"></div>
+  <Dialog :visible="dietary_restrictions_visible" :draggable="false"
+    :header="`${profileInfo.username}'s Dietary Restrictions`" :style="{ width: '50vw' }">
+    <h3>Special Dietary Requirements</h3>
+    <div v-if="profileInfo.dietaryRestrictions">
+      <Tag class="category tag" :value="profileInfo.dietaryRestrictions"></Tag>
+    </div>
+    <div v-else>
+      <p>None</p>
+    </div>
+    <h3 style="margin-top: 10px">Allergies</h3>
+    <div v-if="profileInfo.allergies.length != 0">
+      <Tag v-for="(item, index) in profileInfo.allergies" :key="index" class="category tag" :value="item"></Tag>
+    </div>
+    <div v-else>
+      <p>None</p>
+    </div>
+  </Dialog>
+  <Dialog :visible="statistics_visible" :draggable="false" :header="`${profileInfo.username}'s Activity`"
+    :style="{ width: '50vw' }">
+    <TabsWrapper>
+      <Tab icon="pi-hourglass" title="Carma">
+        <div class="container pt-small">
+          <!-- <div> -->
+          <h3 style="text-align: center;">All-time Carma:<span style="font-size: 1.2rem; color: var(--color-primary)"><i style="margin: 0 5px;"
+                class="pi pi-hourglass"></i>{{ profileInfo.totalCarma }}</span></h3>
+          <h3 style="text-align: center;">Carma this month:<span style="font-size: 1.2rem; color: var(--color-primary)"><i style="margin: 0 5px;"
+                class="pi pi-hourglass"></i>{{ profileInfo.monthlyCarma }}</span></h3>
+          <br />
+          <div>
+            <Chart type="bar" :data="chartData" :options="chartOptions" :canvasProps="{'display': 'flex'}"/>
+          </div>
         </div>
-        <div v-else>
-          <p>None</p>
+      </Tab>
+      <Tab icon="pi-thumbs-up" title="Reviews">
+        <div class="container pt-small">
+          <h3>Reviews</h3>
         </div>
-        <h3 style="margin-top: 10px">Allergies</h3>
-        <div v-if="profileInfo.allergies.length != 0">
-          <Tag v-for="(item, index) in profileInfo.allergies" :key="index"
-              class="category tag"
-              :value="item"
-          ></Tag>
-        </div>
-        <div v-else>
-          <p>None</p>
-        </div>
-    </Dialog>
+      </Tab>
+    </TabsWrapper>
+  </Dialog>
 </template>
 
 <style scoped>
@@ -140,29 +244,34 @@ let toggleModal = () => visible.value = !visible.value;
   align-items: center;
   margin-right: 20px;
 }
+
 .statistics-button {
   background: none;
   color: var(--color-primary);
   border-radius: 100px;
   border-color: var(--color-primary);
 }
+
 .modal {
   top: 0;
   position: fixed;
-  background: rgba(0,0,0,0.5);
+  background: rgba(0, 0, 0, 0.5);
   width: 100%;
   height: 100%;
 }
+
 .tag {
   background-color: var(--color-primary);
   margin-right: 5px;
 }
+
 .edit-button {
   background: var(--color-primary);
   color: white;
   border-radius: 100px;
   border: none;
 }
+
 .profile {
   display: flex;
   width: 100%;
@@ -184,6 +293,7 @@ let toggleModal = () => visible.value = !visible.value;
   width: 600px;
   justify-content: center;
 }
+
 .profile-photo {
   grid-area: profile-photo;
   background: darkgray;
@@ -196,10 +306,12 @@ let toggleModal = () => visible.value = !visible.value;
   background-color: #4caf4f;
   color: #fff;
 }
+
 .profile-info {
   grid-area: profile-info;
   width: fill-available;
 }
+
 .profile-statistics {
   grid-area: statistics;
 }
@@ -212,31 +324,37 @@ let toggleModal = () => visible.value = !visible.value;
     align-items: center;
     width: 100%;
   }
+
   .profile-photo {
     height: 150px;
     width: 150px;
     font-size: 6em;
   }
+
   .icon {
     display: none
   }
+
   .statistics-button {
     font-size: 0;
   }
+
   .statistics-icon-wrapper {
     max-width: 100px;
   }
 }
+
 Button.icon {
-  background: rgba(246,132,56,0.75);
-  color:black;
+  background: rgba(246, 132, 56, 0.75);
+  color: black;
   border-radius: 100%;
   outline: none;
   border: none;
   margin-right: 15px;
 }
+
 Button.dietary-restrictions {
-  color:var(--color-primary);
+  color: var(--color-primary);
   border-color: var(--color-primary);
   background: none;
   border-radius: 50px;
@@ -250,7 +368,7 @@ Overriding PrimeVue's tab component styles
   display: grid;
   justify-items: center;
 }
-.tabview-custom > a {
+
+.tabview-custom>a {
   width: 170px !important;
-}
-</style>
+}</style>
